4023524f4ecd810f00fe5e7d13ca4e52b97dbbb5
Patrick Stein <lpstein@yahoo-inc.com>
2014-08-01 13:11:36 -0700

    Fix screwdriver config

--- script/lint.sh
+++ script/lint.sh
@@ -7,7 +7,7 @@ export FILE_COUNT=`find lib -name "*.js" | wc -l | tr -d ' '`
 
 if [ -n "$LINT_OUTPUT_DIR" ]; then
   rm -rf ./$LINT_OUTPUT_DIR/*
-  node_modules/.bin/jshint lib --reporter ./test/util/jshint_reporter >"$LINT_OUTPUT_DIR/yhint.json"
+  node_modules/.bin/jshint lib --reporter ./test/unit/util/jshint_reporter >"$LINT_OUTPUT_DIR/yhint.json"
 else
   node_modules/.bin/jshint lib
 fi
