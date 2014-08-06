65f036164539a7e9435cdeb68cd4caf728a0d38f
Patrick Stein <lpstein@yahoo-inc.com>
2014-08-06 14:00:36 -0700

    Scope interpretation pass; lambda support

--- test/unit/scope.js
+++ test/unit/scope.js
@@ -4,25 +4,48 @@ var types = require('./util/require')('types')
   ;
 
 var calc = function(str) {
-  return scope.build(parser(str)[0]);
+  return scope.build(parser.snippet(str));
 };
 
-// exports.testBasic = function(test, assert) {
-//   var scope = calc('function foo { let a = 1; let b = 2}');
+exports.testBasic = function(test, assert) {
+  var scope = calc('function foo { let a = 1; let b = 2}');
 
-//   assert.isDefined(scope.variables.a);
-//   assert.isDefined(scope.variables.b);
+  assert.isDefined(scope.functions.foo);
+  assert.equal(scope.scopes.length, 1);
+  assert.equal(scope.scopes[0].parent, scope);
+  assert.isDefined(scope.scopes[0].variables.a);
+  assert.isDefined(scope.scopes[0].variables.b);
 
-//   test.finish();
-// };
+  test.finish();
+};
 
-// exports.testBasic = function(test, assert) {
-//   var scope = calc('function foo { let a = 1; function nested { let b = 2 } }');
+exports.testAllVariableDeclarations = function(test, assert) {
+  var scope = calc('let a = 1; out b = 1; mut c = 1; let d = 1');
 
-//   assert.isDefined(scope.variables.a);
-//   assert.isDefined(scope.functions.nested);
-//   assert.equal(scope.scopes.length, 1);
-//   assert.isDefined(scope.scopes[0].variables.b);
+  assert.isDefined(scope.variables.a);
+  assert.isDefined(scope.variables.b);
+  assert.isDefined(scope.variables.c);
 
-//   test.finish();
-// };
+  test.finish();
+};
+
+exports.testStruct = function(test, assert) {
+  var scope = calc('struct Foo {}');
+
+  assert.isDefined(scope.structs.Foo);
+
+  test.finish();
+};
+
+exports.testNested = function(test, assert) {
+  var scope = calc('function foo { let a = 1; function nested { let b = 2 } }');
+
+  assert.equal(scope.scopes.length, 1);
+  assert.equal(scope.scopes[0].scopes.length, 1);
+  assert.isDefined(scope.functions.foo);
+  assert.isDefined(scope.scopes[0].functions.nested);
+  assert.isDefined(scope.scopes[0].variables.a);
+  assert.isDefined(scope.scopes[0].scopes[0].variables.b);
+
+  test.finish();
+};
