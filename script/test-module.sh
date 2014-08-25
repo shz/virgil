45cf6145d8859353baa9d7ae496e6c5108c7d0d3
Patrick Stein <lpstein@yahoo-inc.com>
2014-08-25 11:10:24 -0700

    Basics imports working for JS

---
+++ script/test-module.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Canonical dir
+cd "$(dirname $0)/../"
+
+# Args
+debug=0
+
+for var in "$@"
+do
+  if [ $var == '--debug' ]; then
+    debug=1
+  else
+    echo ''
+    echo "Unknown option $var"
+    echo ''
+  fi
+done
+
+# Colors
+RESTORE='\033[0m'
+
+RED='\033[00;31m'
+GREEN='\033[00;32m'
+YELLOW='\033[00;33m'
+BLUE='\033[00;34m'
+PURPLE='\033[00;35m'
+CYAN='\033[00;36m'
+LIGHTGRAY='\033[00;37m'
+
+LRED='\033[01;31m'
+LGREEN='\033[01;32m'
+LYELLOW='\033[01;33m'
+LBLUE='\033[01;34m'
+LPURPLE='\033[01;35m'
+LCYAN='\033[01;36m'
+WHITE='\033[01;37m'
+
+file="test/module/main.vgl"
+printf "${YELLOW}$file${RESTORE}  =  "
+result=`bin/virgil-js $file -o test/module/output 2>&1`
+if [ $? -ne 0 ]; then
+  echo "${RED}FAIL${RESTORE}"
+  # echo '--------------------------------------------------------'
+  echo "$result"
+  echo ''
+  echo ''
+else
+  echo "${GREEN}PASS${RESTORE}"
+
+  if [ $debug -eq 1 ]; then
+    echo "$result"
+  fi
+fi
+echo ''
+
