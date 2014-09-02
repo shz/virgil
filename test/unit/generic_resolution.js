232b6704942a167947bb3a585ac8d84d04b77966
Patrick Stein <lpstein@yahoo-inc.com>
2014-09-02 13:57:52 -0700

    Generic resolution

---
+++ test/unit/generic_resolution.js
@@ -0,0 +1,57 @@
+var types = require('./util/require')('types')
+  , parser = require('./util/require')('parser')
+  , passes = require('./util/require')('passes')
+  ;
+
+var TR = types.TypeRef;
+var resolve = types.generics.resolve;
+
+exports.testBasic = function(test, assert) {
+  var r = resolve( new TR("int")
+                 , types.canned['int']
+                 );
+  assert.equal(Object.keys(r).length, 0);
+
+  var r = resolve( new TR("'T")
+                 , types.canned['int']
+                 );
+  assert.equal(Object.keys(r).length, 1);
+  assert.isDefined(r["'T"]);
+  assert.ok(types.equal(r["'T"], types.canned['int']));
+
+  var r = resolve( new TR("'T")
+                 , new TR('list', [types.canned['int']])
+                 );
+  assert.equal(Object.keys(r).length, 1);
+  assert.isDefined(r["'T"]);
+  assert.ok(types.equal(r["'T"], new TR('list', [types.canned['int']])));
+
+  var r = resolve( new TR('list', [new TR("'T")])
+                 , new TR('list', [new TR('int')])
+                 );
+  assert.equal(Object.keys(r).length, 1);
+  assert.isDefined(r["'T"]);
+  assert.ok(types.equal(r["'T"], types.canned['int']));
+
+  // Straight up type mismatches are a problem
+  assert.throws(function() {
+    resolve(types.canned['int'], types.canned['str']);
+  }, /equal/i);
+
+  // As are generics with matching signatures but different types
+  assert.throws(function() {
+    resolve( new TR('list', [new TR("'T")])
+           , new TR('func', [types.canned['str']])
+           );
+  }, /mismatch/i);
+
+  // Generics of the same type with different number of params won't
+  // work either.
+  assert.throws(function() {
+    resolve( new TR('list', [new TR("'T"), types.canned['str']])
+           , new TR('list', [types.canned['str']])
+           );
+  }, /mismatch/i);
+
+  test.finish();
+};
