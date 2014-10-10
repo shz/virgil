a7878dea218fb9d0cbcb1e2d54989688a1a538ab
Patrick Stein <lpstein@yahoo-inc.com>
2014-10-10 16:32:13 -0700

    Whoops, forgot to include files :S

---
+++ test/unit/generics_util.js
@@ -0,0 +1,6 @@
+var TypeRef = require('./util/require')('types/typeref')
+  , generics = require('./util/require')('types/generics')
+  ;
+
+
+// TODO - Most things
---
+++ test/unit/return_statement_checking.js
@@ -0,0 +1,29 @@
+var types = require('./util/require')('types')
+  , parser = require('./util/require')('parser')
+  , passes = require('./util/require')('passes')
+  ;
+
+var parse = function(str) {
+  passes.runAll(parser.snippet(str));
+};
+
+exports.testEmpty = function(test, assert) {
+  // These shouldn't fail
+  parse('function a {}');
+  parse('method foo(a : int) {}');
+  parse('lambda {}');
+  parse('function a { return void }');
+  parse('method foo(a : int) { return void }');
+  parse('lambda { return void }');
+
+  assert.throws(function() {
+    parse('function a : int {}');
+    parse('method foo(a : int) : int {}');
+    parse('lambda : int {}');
+    parse('function a : int { return void }');
+    parse('method foo(a : int) : int { return void }');
+    parse('lambda : int { return void }');
+  }, /retur/);
+
+  test.finish();
+};
