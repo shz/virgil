8a15598fa0bfdb90706a24ad725044dc393b10c3
Patrick Stein <lpstein@yahoo-inc.com>
2015-02-12 18:33:16 -0800

    Rename variables in JS output to handle scope differences between Virgil and JS [Fix #49]

--- test/unit/converters/javascript/scope.js
+++ test/unit/converters/javascript/scope.js
@@ -16,9 +16,14 @@ var convert = function(str) {
 exports.testVariableRenaming = function(test, assert) {
   // Make sure shadowed variables aren't renamed superfluously
   assert.match(convert('let a = 1; function test { let! a = 2 }'), /a\s[\s\S]*a\s/);
+  assert.match(convert('let a = 1; let b = lambda { let! a = 1 }'), /a\s[\s\S]*a\s/);
+  assert.match(convert('let a = 1; method test(z : int) { let! a = 1 }'), /a\s[\s\S]*a\s/);
 
   // Make sure shadowed variables outside function scope ARE renamed
-  assert.match(convert('let a = 1; for i = 0 upto 1 { let! a = 2 }'), /a[\s\S]*a1/);
+  assert.match(convert('let a = 1; for i = 0 upto 1 { let! a = 2 }'), /a[\s\S]*a\$1/);
+
+  // Make sure renamed variables are accessed by the corrent name
+  assert.match(convert('let z = 1; for i = 0 upto 1 { mut! z = 2; z = 4 }'), /z\$1[\s\S]*z\$1/);
 
   test.finish();
 };
