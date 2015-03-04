3b6056ca169943372dcc29cf3cedf998a2560eec
dfsklar <dfsklar@gmail.com>
2015-03-03 18:08:33 -0800

    Alpha versions of the DateTime methods including localized support for date-time formatting.

--- runtime/javascript/datetime.js
+++ runtime/javascript/datetime.js
@@ -1,7 +1,61 @@
 module.exports = function DateTime(spec) {
-  this.ts = (spec && typeof spec.ts == 'number') ? spec.ts : +new Date();
+  this.ts = (spec && typeof spec.ts == 'number') ? spec.ts : ((new Date() / 1000) | 0);
   this.offset = (spec && typeof spec.offset == 'number') ? spec.offset : 0;
 };
 
-// module.exports.prototype.toLocal   
+var DateTime = module.exports;
 
+module.exports.prototype.toGMT = function() {
+  return new DateTime({ts: this.ts, offset: 0});
+}
+
+module.exports.prototype.toLocal = function() {
+  return new DateTime({ts: this.ts, offset: new Date().getTimezoneOffset() * 60});
+}
+
+module.exports.prototype.toOffset = function(newoffset) {
+  return new DateTime({ts: this.ts, offset: newoffset});
+}
+
+function canUseInternationalizationAPI() {
+  var jsDate = new Date();
+  return (typeof(jsDate.toLocaleDateString) == 'function') && (jsDate.toLocaleDateString('en-US') != jsDate.toLocaleDateString('de-AT'));
+}
+
+
+/* 
+
+Currently, any defined value for specForDate/Time means "show that particular component".
+Eventually, we will support variations such as "short", "long", etc.
+
+We have two formatters -- we deploy the one that best uses the JS environment's internationalization capabilities.
+*/
+
+function formatSophisticated (specForDate, specForTime) {
+  var retval = "";
+  var jsDate = new Date(this.ts + this.offset);
+  if (specForDate) {
+    retval = jsDate.toLocaleDateString();  // offset has already been done via manip of the timestamp
+  }
+  if (specForTime) {
+    retval += (retval ? " " : "") + jsDate.toLocaleTimeString();  // offset has already been done via manip of the timestamp
+  }
+  return retval;
+};
+
+function formatFallback (specForDate, specForTime) {
+  var retval = "";
+  var jsDate = new Date(this.ts + this.offset);
+  if (specForDate) {
+    // Right now: just american style mm/dd/yyyy
+    // We will want to do a better job, more sensitive to the major other locales, soon.
+    retval = (jsDate.getMonth()+1) + "/" + (jsDate.getDay()+1) + "/" + (jsDate.getYear()+1900);  // offset has already been done via manip of the timestamp
+  }
+  if (specForTime) {
+    // Right now: just a basic hh:mm:ss in military time
+    retval += (retval ? " " : "") + jsDate.toLocaleTimeString();  // offset has already been done via manip of the timestamp
+  }
+  return retval;
+};
+
+module.exports.prototype.format = canUseInternationalizationAPI() ? formatSophisticated : formatFallback;
--- test/unit/runtime/javascript/datetime.js
+++ test/unit/runtime/javascript/datetime.js
@@ -1,6 +1,39 @@
 var DateTime = require('../../../../runtime/javascript/datetime');
 
 test('unit', 'runtime', 'javascript', 'DateTime', 'constructor', function() {
+  var ahora = +new Date();
   var dt = new DateTime();
-  assert(false);
+  assert.equal(dt.offset, 0);
+
+  // The difference between the two timestamps should really not be exceeding one second, in most cases will be zero of course.
+  assert((dt.ts - ahora) < 2);
+
+  var dt2 = new DateTime(dt);
+  assert.equal(dt.ts, dt2.ts);
+  assert.equal(dt.offset, dt2.offset);
 });  
+
+test('unit', 'runtime', 'javascript', 'DateTime', 'localization', function() {
+  var timestampFixed = 1425430319083;
+
+  var dtUTC = new DateTime({ts: timestampFixed});
+  var strdtUTC = dtUTC.format('short', 'short');
+
+  // This assumes NodeJS v0.10.x -- upon move to newest NodeJS, this test will need mods.
+  assert.equal("3/3/2015 16:51:59", strdtUTC);
+
+  var dtLocal = dtUTC.toLocal();
+  var strdtLocal = dtLocal.format('short', 'short');
+
+  // This assumes we are not running this test in Merrie Olde England :-) !
+  assert("3/3/2015 16:51:59" != strdtLocal);
+
+  var strdtLocalTimeOnly = dtLocal.format(null, 'short');
+  assert(strdtLocalTimeOnly.length < strdtLocal.length);
+
+  var strdtLocalDateOnly = dtLocal.format("short", null);
+  assert(strdtLocalDateOnly.length < strdtLocal.length);
+  assert(strdtLocalDateOnly != strdtLocalTimeOnly);
+
+});
+
