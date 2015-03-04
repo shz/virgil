824aa5f9ea15fc824f67b288c9cbbc3aa57e065d
Cedric Han <cedrich@yahoo-inc.com>
2015-03-04 11:39:21 -0800

    Add tests for method naming

--- test/unit/converters/javascript/scope.js
+++ test/unit/converters/javascript/scope.js
@@ -25,3 +25,19 @@ test('unit', 'converters', 'javascript', 'scope', 'variable renaming', function(
   // Make sure renamed variables are accessed by the corrent name
   assert.match(convert('let z = 1; for i = 0 upto 1 { mut! z = 2; z = 4 }'), /z\$1[\s\S]*z\$1/);
 });
+
+
+test('unit', 'converters', 'javascript', 'scope', 'unnatural method renaming with generics', function() {
+  assert(convert("export method bar(foo:Foo) {}").indexOf('bar$Foo') >= 0);
+
+  // Angle braces, quot, and commas convert to underscores
+  assert(convert("export method bar(foo:Foo<'B>) {}").indexOf('bar$Foo__B_') >= 0);
+  assert(convert("export method bar(foo:Foo<'B,'T>) {}").indexOf('bar$Foo__B__T_') >= 0);
+
+  // Spaces between types get dropped
+  assert(convert("export method bar(foo:Foo<'B, 'T>) {}").indexOf('bar$Foo__B__T_') >= 0);
+  assert(convert("export method bar(foo:Foo<Strukt, Strukt2>) {}").indexOf('bar$Foo_Strukt_Strukt2') >= 0);
+
+  // Other parameters don't count in naming
+  assert(convert("export method bar(foo:Foo<'B, 'T>, garply:int) {}").indexOf('bar$Foo__B__T') >= 0);
+});
