103501e3dc0a8320c262d5b881dba5c04a6e4f05
Patrick Stein <lpstein@yahoo-inc.com>
2015-03-31 18:16:34 -0700

    While loop, some of for loop

--- test/functional/interpreter.js
+++ test/functional/interpreter.js
@@ -60,13 +60,30 @@ test('functional', 'interpreter', 'variables', function() {
 test('functional', 'interpreter', 'control flow', function() {
   var result;
 
+  // If statement, passed
   result = interpreter.run(buildFunc('function main : int { let a = 1 + 3; if a > 1 { return 2 } else { return 5 } }'));
   assert.isDefined(result);
   assert.ok(result instanceof ast.IntegerLiteral);
   assert.equal(result.value, 2);
 
+  // If statement, else
   result = interpreter.run(buildFunc('function main : int { let a = 1 + 3; if a > 100 { return 2 } else { return 5 } }'));
   assert.isDefined(result);
   assert.ok(result instanceof ast.IntegerLiteral);
   assert.equal(result.value, 5);
+
+  // While loop, runs, returns 1
+  result = interpreter.run(buildFunc('function main : int { while true { return 1 } }'));
+  assert.isDefined(result);
+  assert.ok(result instanceof ast.IntegerLiteral);
+  assert.equal(result.value, 1);
+
+  // While loop, does not run
+  result = interpreter.run(buildFunc('function main : int { while false { return 1 } return 4 }'));
+  assert.isDefined(result);
+  assert.ok(result instanceof ast.IntegerLiteral);
+  assert.equal(result.value, 4);
+
+  // TODO - While loop, accum
+  // TODO - For loop in all its forms
 });
