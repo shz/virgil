8eb9d3934698a15c2117c3f28aebd56f5db21431
Patrick Stein <lpstein@yahoo-inc.com>
2015-04-21 16:07:05 -0700

    Implement mini browserify

---
+++ test/unit/converters/javascript/browserify_mini.unit.js
@@ -0,0 +1,54 @@
+var browserifyMini = require('../../../../lib/converters/javascript/browserify_mini');
+
+var run = function(filemap, entrypoint) {
+  var src = browserifyMini(filemap, entrypoint);
+  return eval(src);
+};
+
+test('unit', 'converters', 'javascript', 'browserify mini', 'basic', function() {
+  var filemap = {
+    'a.js': 'var b = require("./b.js"); exports.message = function() { return "hello world" };',
+    'b.js': ''
+  };
+
+  var result = run(filemap, 'a.js');
+  assert.isDefined(result);
+  assert.isDefined(result.message);
+  assert.equal(typeof result.message, 'function');
+  assert.equal(result.message(), 'hello world');
+});
+
+test('unit', 'converters', 'javascript', 'browserify mini', 'bad file', function() {
+  var filemap = {
+    'a.js': 'require("./b.js");',
+    'b.js': 'require("./c.js");'
+  };
+
+  assert.throws(function() {
+    run(filemap, 'a.js');
+  }, /not in/i);
+});
+
+test('unit', 'converters', 'javascript', 'browserify mini', 'circular', function() {
+  var filemap = {
+    'a.js': 'require("./b.js");',
+    'b.js': 'require("./a.js");'
+  };
+
+  assert.throws(function() {
+    run(filemap, 'a.js');
+  }, /circular/i);
+});
+
+test('unit', 'converters', 'javascript', 'browserify mini', 'complex', function() {
+  var filemap = {
+    'a.js': 'var b = require("./b.js"); exports.message = function() { return "hello world" };',
+    'b.js': ''
+  };
+
+  var result = run(filemap, 'a.js');
+  assert.isDefined(result);
+  assert.isDefined(result.message);
+  assert.equal(typeof result.message, 'function');
+  assert.equal(result.message(), 'hello world');
+});
